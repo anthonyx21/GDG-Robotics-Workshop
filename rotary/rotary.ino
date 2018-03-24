19f2ada85ede04d38313070a0cfa7a247dcb7e07
Nguyen, Anthony <Anthony.Nguyen@td.com>
2018-03-24 14:01:10 -0400

    initial commit

---
+++ rotary/rotary.ino
@@ -0,0 +1,74 @@
+/*     Arduino Rotary Encoder Tutorial
+ *      
+ *  by Dejan Nedelkovski, www.HowToMechatronics.com
+ *  
+ */
+ 
+ #define outputA 6
+ #define outputB 7
+ #define buttonPin 5
+
+ class Button {
+    private:
+        bool _state;
+        uint8_t _pin;
+
+    public:
+        Button(uint8_t pin) : _pin(pin) {}
+
+        void begin() {
+            pinMode(_pin, INPUT_PULLUP);
+            _state = digitalRead(_pin);
+        }
+
+        bool isReleased() {
+            bool v = digitalRead(_pin);
+            if (v != _state) {
+                _state = v;
+                if (_state) {
+                    return true;
+                }
+            }
+            return false;
+        }
+};
+
+Button myButton(buttonPin);
+
+ int counter = 0; 
+ int aState;
+ int aLastState;  
+ void setup() { 
+   pinMode (outputA,INPUT);
+   pinMode (outputB,INPUT);
+//   pinMode (buttonPin,INPUT);
+    myButton.begin();
+   
+   Serial.begin (9600);
+   // Reads the initial state of the outputA
+   aLastState = digitalRead(outputA);   
+ } 
+ void loop() { 
+   aState = digitalRead(outputA); // Reads the "current" state of the outputA
+   // If the previous and the current state of the outputA are different, that means a Pulse has occured
+   if (aState != aLastState){     
+     // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
+     if (digitalRead(outputB) != aState) { 
+       counter ++;
+     } else {
+       counter --;
+     }
+     Serial.print("Position: ");
+     Serial.println(counter);
+   } 
+   aLastState = aState; // Updates the previous state of the outputA with the current state
+
+//   if(digitalRead(buttonPin)){
+//    
+//     Serial.println("button press");
+//   }
+    if (myButton.isReleased()) {
+        Serial.println(F("Released"));
+    }
+   
+ }
